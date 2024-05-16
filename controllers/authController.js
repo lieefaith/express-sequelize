ccf5a00c97e23be09e4796c1435355ed55765c66
alamsyahalamm <muhammad728744@smk.belajar.id>
2024-05-16 15:42:24 +0700

    add express-sequelize

---
+++ controllers/authController.js
@@ -0,0 +1,112 @@
+const { users } = require("../models");
+const generateToken = require("../config/generateToken");
+const { comparePassword, hashPassword } = require("../config/bcrypt");
+const {
+  errorResponse,
+  successResponse,
+  internalErrorResponse,
+  notFoundResponse,
+} = require("../config/responseJson");
+const { users } = require("../models");
+
+async function register(req, res) {
+  try {
+    const { username, email, password } = req.body;
+    // Check if user already exists
+    const existingEmail = await users.findOne({ where: { email } });
+    if (existingEmail) {
+      errorResponse(res, "Email already exists", 400);
+    }
+
+    // Hash the password
+    const hashedPassword = await hashPassword(password);
+
+    // Create new user
+    const user = await users.create({
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+    const userResponse = {
+      id: user.id,
+      username: user.username,
+      email: user.email,
+      createdAt: user.createdAt,
+      updatedAt: user.updatedAt,
+    };
+
+    successResponse(res, "User registered successfully", userResponse, 201);
+  } catch (error) {
+    internalErrorResponse(res, error);
+  }
+}
+
+async function login(req, res) {
+  try {
+    const { email, password } = req.body;
+
+    // Check if user exists
+    const user = await users.findOne({ where: { email } });
+    if (!user) {
+      notFoundResponse(res, "User not found");
+    }
+
+    // Validate password
+    const validPassword = await comparePassword(password, user.password);
+    if (!validPassword) {
+      errorResponse(res, "Invalid password", 401);
+    }
+
+    const userResponse = {
+      id: user.id,
+      username: user.username,
+      email: user.email,
+    };
+
+    const token = generateToken(user);
+    successResponse(
+      res,
+      "Logged in successfully",
+      {
+        user: userResponse,
+        token,
+      },
+      200
+    );
+  } catch (error) {
+    console.error("Error logging in user:", error);
+    internalErrorResponse(res, error);
+  }
+}
+
+async function me(req, res) {
+  try {
+    const user = await users.findByPk(req.user.id, {
+      attributes: ["id", "username", "email"],
+    });
+    if (!user) {
+      errorResponse(res, "User not found", 404);
+    }
+    successResponse(res, "User fetched successfully", user, 200);
+  } catch (error) {
+    console.error("Error fetching user:", error);
+    internalErrorResponse(res, error);
+  }
+}
+
+async function logout(req, res) {
+  try {
+    successResponse(res, "Logged out successfully", null, 200);
+  } catch (error) {
+    console.error("Error logging out user:", error);
+    internalErrorResponse(res, error);
+  }
+}
+
+module.exports = {
+  register,
+  login,
+  me,
+  logout,
+};
